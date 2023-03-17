92d1a4a4eb3813f396dffdb055d46efac24e34a5
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-03-17 14:56:37 +0100

    finished sidebar

--- src/UI/sidebar.js
+++ src/UI/sidebar.js
@@ -3,6 +3,7 @@ const storage = new LocalStorageOperations();
 
 export default function createSideBar () {
     const content = document.createElement('div');
+    const sideBarStuff = document.createElement('div')
 
     const allTasksContainer = document.createElement('div')
     const todayContainer = document.createElement('div')
@@ -11,33 +12,47 @@ export default function createSideBar () {
 
     content.classList.add('side-bar-content')
 
+    sideBarStuff.classList.add('side-bar-stuff')
+
     allTasksContainer.innerHTML = 
-    '<i></i> \
+    '<i class="fa-solid fa-list-check" ></i> \
     <div class"all-tasks" >All tasks</div>';
 
     todayContainer.innerHTML = 
-    '<i></i> \
+    '<i class="fa-solid fa-calendar-week" ></i> \
     <div class"today-tasks-btn" >Today</div>';
 
     next7DaysContainer.innerHTML =
-    '<i></i> \
+    '<i class="fa-regular fa-calendar-days"></i> \
     <div class"nsd-tasks-btn" >Next 7 days</div>';
 
     projectsContainer.innerHTML =
-    `<div><i></i><div class="projects">Projects</div></div>`;
+    `<div> \
+        <i class="fa-solid fa-briefcase" ></i> \
+        <div class="projects">Projects</div> \
+        <i class="fas fa-caret-down" ></i> \
+    </div>`;
     projectsContainer.appendChild(makeProjects())
+    projectsContainer.setAttribute('id', 'projects-container')
+    projectsContainer.addEventListener('click', () => {
+        const projectList = document.getElementsByClassName('project-list')[0]
+        console.log(projectList)
+        projectList.classList.toggle('display');
+    })
 
-    content.appendChild(allTasksContainer)
-    content.appendChild(todayContainer)
-    content.appendChild(next7DaysContainer)
-    content.appendChild(projectsContainer)
+    sideBarStuff.appendChild(allTasksContainer)
+    sideBarStuff.appendChild(todayContainer)
+    sideBarStuff.appendChild(next7DaysContainer)
+    sideBarStuff.appendChild(projectsContainer)
 
+    content.appendChild(sideBarStuff)
     return content;
 }
 
 function makeProjects() {
     const projectList = document.createElement('ul');
     projectList.classList.add('project-list')
+    projectList.classList.add('hide-projects')
 
     const { projects } = storage.getAll();
     projects.forEach(project => {
